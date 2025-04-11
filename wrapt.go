0641566fd0a7231f764343db97624cddcc903338
onioncall <orion.callaghan@gmail.com>
2025-04-10 20:37:37 -0400

    added array support

--- wrapt.go
+++ wrapt.go
@@ -8,7 +8,7 @@ import (
 	"golang.org/x/term"
 )
 
-
+// Returns a string, with line breaks for each line
 func Wrap(s string) string {
 	w, _, err := term.GetSize(int(os.Stdout.Fd()))
 	if err != nil {
@@ -36,6 +36,44 @@ func Wrap(s string) string {
 	}
 
 	return s
+}
+
+// Returns an array of strings, representing each line to be printed
+func WrapArray(s string, tabsize int) []string {
+	w, _, err := term.GetSize(int(os.Stdout.Fd()))
+	if err != nil {
+		fmt.Println("Error getting terminal size:", err)
+	}	
+
+	w -= tabsize
+
+	arr := []string{}
+	if len(s) < w {
+		return append(arr, s)
+	}
+
+	startingPos := 0
+	endingPos := 0
+	for range 100000 {
+		// if we don't contain a breaking character, return early
+		if !strings.Contains(s[startingPos:], " ") {
+			fmt.Println("break from contains")
+			break
+		}
+		if startingPos + w > len(s) - 1 {
+			w = len(s[startingPos:])
+		} 	
+
+		endingPos += w
+		endingPos = lastSpaceBeforePosition(s, endingPos)
+
+		line := s[startingPos:endingPos]
+		startingPos = endingPos + 1
+
+		arr = append(arr, line)
+	}
+
+	return arr
 }
 
 func lastSpaceBeforePosition(s string, pos int) int {
