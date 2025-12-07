2ac9cf3442027d007ba86a818926f529beb7a70e
onioncall <orion.callaghan@gmail.com>
2025-12-06 23:22:40 -0600

    resetting cursor on actual line breaks

--- example/main.go
+++ example/main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	s := "The quick brown fox jumps over the lazy dog. Bright vixens jump dozy fowl quack. My jocks box, get hard, very numb. Crazy Fredrick bought many very exquisite opal jewels. How razorback-jumping frogs can level six piqued gymnasts. Jaded zombies acted quaintly but kept driving their oxen forward. Sphinx of black quartz, judge my vow. Pack my box with five dozen liquor jugs. The five boxing wizards jump quickly. Jackdaws love my big sphinx of quartz. Five quacking zephyrs jolt my wax bed. Waltz, bad nymph, for quick jigs vex. Quick zephyrs blow, vexing daft Jim. How quickly daft jumping zebras vex. Two driven jocks help fax my big quiz. Sympathizing would fix quaker objectives. Pack my red box with five dozen quality jugs. Amazingly few discotheques provide jukeboxes. Junk MTV quiz graced by fox whelps."
+	s := "The quick brown fox jumps over the lazy dog.\nBright vixens jump dozy fowl quack. My jocks box, get hard, very numb. Crazy Fredrick bought many very exquisite opal jewels. How razorback-jumping frogs can level six piqued gymnasts. Jaded zombies acted quaintly but kept driving their oxen forward. Sphinx of black quartz, judge my vow. Pack my box with five dozen liquor jugs. The five boxing wizards jump quickly. Jackdaws love my big sphinx of quartz. Five quacking zephyrs jolt my wax bed. Waltz, bad nymph, for quick jigs vex. Quick zephyrs blow, vexing daft Jim. How quickly daft jumping zebras vex. Two driven jocks help fax my big quiz. Sympathizing would fix quaker objectives. Pack my red box with five dozen quality jugs. Amazingly few discotheques provide jukeboxes. Junk MTV quiz graced by fox whelps."
 
 	w, _, err := term.GetSize(int(os.Stdout.Fd()))
 	if err != nil {
@@ -20,12 +20,12 @@ func main() {
 	fmt.Println(s)
 	fmt.Print("\n")
 	fmt.Print("----------------------\n\n")
-	
+
 	fmt.Print("Wrap\n\n")
 	fmt.Println(lw.Wrap(s, w))
 	fmt.Print("\n")
 	fmt.Print("----------------------\n\n")
-	
+
 	fmt.Print("WrapArray\n\n")
 	for _, s := range lw.WrapArray(s, 4, w) {
 		fmt.Printf("    %s\n", s)
--- wrapt.go
+++ wrapt.go
@@ -15,17 +15,26 @@ func Wrap(text string, terminalWidth int) string {
 	pos := 0
 	// we're going to set an upper bound for this loop
 	for range 10000 {
+		// Check for actual line breaks in the upcoming section
+		nextNewline := strings.Index(text[pos:], "\n")
+
+		// If there's a newline before our next wrap point, reset from there
+		if nextNewline != -1 && nextNewline < terminalWidth {
+			pos += nextNewline + 1 // Resetting curson after newline
+			continue
+		}
+
 		// if we don't contain a breaking character, return early
 		if !strings.Contains(text[pos:], " ") {
 			break
 		}
-		if pos + terminalWidth > len(text) - 1 {
+		if pos+terminalWidth > len(text)-1 {
 			break
-		} 	
+		}
 		pos += terminalWidth
 
 		pos = lastSpaceBeforePosition(text, pos)
-		text = insertBreak(text, pos + 1)
+		text = insertBreak(text, pos+1)
 	}
 
 	return text
@@ -36,53 +45,53 @@ func Wrap(text string, terminalWidth int) string {
 // terminalWidth: terminal width
 // Returns an array of strings, representing each line to be printed
 func WrapArray(text string, marginSize int, terminalWidth int) []string {
-    terminalWidth -= marginSize
+	terminalWidth -= marginSize
 
-    arr := []string{}
-    if len(text) < terminalWidth {
-        return append(arr, text)
-    }
+	arr := []string{}
+	if len(text) < terminalWidth {
+		return append(arr, text)
+	}
 
-    startingPos := 0
-    
-    for range 10000 {
-        if startingPos >= len(text) {
-            break
-        }
-        
-        endingPos := startingPos + terminalWidth
-        if endingPos >= len(text) {
-            arr = append(arr, text[startingPos:])
-            break
-        }
-        
-        endingPos = lastSpaceBeforePosition(text, endingPos)
-        if endingPos == -1 || endingPos <= startingPos {
-            arr = append(arr, text[startingPos:])
-            break
-        }
-        
-        line := text[startingPos:endingPos]
-        arr = append(arr, line)
-        startingPos = endingPos + 1
-    }
-    
-    return arr
+	startingPos := 0
+
+	for range 10000 {
+		if startingPos >= len(text) {
+			break
+		}
+
+		endingPos := startingPos + terminalWidth
+		if endingPos >= len(text) {
+			arr = append(arr, text[startingPos:])
+			break
+		}
+
+		endingPos = lastSpaceBeforePosition(text, endingPos)
+		if endingPos == -1 || endingPos <= startingPos {
+			arr = append(arr, text[startingPos:])
+			break
+		}
+
+		line := text[startingPos:endingPos]
+		arr = append(arr, line)
+		startingPos = endingPos + 1
+	}
+
+	return arr
 }
 
 func lastSpaceBeforePosition(s string, pos int) int {
-    for i := pos - 1; i >= 0; i-- {
-        if s[i] == ' ' {
-            return i
-        }
-    }
-    
-    return -1
+	for i := pos - 1; i >= 0; i-- {
+		if s[i] == ' ' {
+			return i
+		}
+	}
+
+	return -1
 }
 
 func insertBreak(s string, pos int) string {
-    before := s[:pos]
-    after := s[pos:]
+	before := s[:pos]
+	after := s[pos:]
 
 	return before + "\n" + after
 }
